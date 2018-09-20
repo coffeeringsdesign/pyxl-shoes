8800292c151bb9fe9654d0bc4630f40e0b2663e4
Kendra Kelly and Luke Vandekieft <luke@>
2018-09-20 11:55:31 -0700

    get responsiveness to work work shoe grid, begin lightbox javascript attempt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 import './sass/styles.scss';
 
-console.log("heyyy there!");
+// $(".shoeThumbnail").click(function() {
+//    $(".overlay").show();
+//    $(".video").show();
+//  });
+
+ var buttons = document.querySelectorAll("button");
+
+ buttons.forEach(function(item) {
+   item.onclick = function(e) {
+    while (imageHolder.firstChild) {
+      imageHolder.removeChild(imageHolder.firstChild);
+    }
+    document.getElementById('overlay').style.display = 'block';
+    document.getElementById('imageHolder').innerHTML = this.innerHTML;
+   }
+ });
+
+ // buttons.forEach(function(item) {
+ //   item.onclick = function(e) {
+ //     $("#imageHolder").empty();
+ //     $(".overlay").show();
+ //     document.getElementById('imageHolder').innerHTML = this.innerHTML;
+ //   }
+ // });
+
+ // $("#cheat-sheet-button").click(function() {
+ //   $(".overlay").show();
+ //   $(".cheat-sheet").show();
+ // })
+ //
+ // $(".close-overlay").click(function() {
+ //   $(".overlay").hide();
+ //   $(".video").hide();
+ //   $(".cheat-sheet").hide();
+ // })
