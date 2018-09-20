f3b1cd0038deaea51b0f25e8a642ff16b37eb8d8
Kendra Kelly and Luke Vandekieft <kendra@>
2018-09-20 16:17:16 -0700

    finalize reponsiveness and add in menu and arrow anchor tag

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import './sass/styles.scss';
 
-// $(".shoeThumbnail").click(function() {
-//    $(".overlay").show();
-//    $(".video").show();
-//  });
+function splitString(userText) {
+  return userText.split(/(\.)/);
+}
 
  var buttons = document.querySelectorAll("button");
 
@@ -12,26 +11,20 @@ import './sass/styles.scss';
     while (imageHolder.firstChild) {
       imageHolder.removeChild(imageHolder.firstChild);
     }
-    document.getElementById('overlay').style.display = 'block';
+    document.getElementById('overlay').style.display = 'flex';
+    var imageText = this.innerHTML;
+    console.log(this.innerHTML);
     document.getElementById('imageHolder').innerHTML = this.innerHTML;
+//THIS SECTION DOES NOT WORK BECAUSE WEBPACK WON'T LOAD 'UNAVAILABLE' IMAGES NOT IN HTML: THIS SOLUTION WORKS WITHOUT WEBPACK.
+    // var wordArray = splitString(imageText);
+    // wordArray.splice(1, 0, "-large");
+    // console.log(wordArray);
+    // var finalImage = wordArray.join("");
+    // console.log(finalImage);
+    // document.getElementById('imageHolder').innerHTML = finalImage;
    }
  });
 
- // buttons.forEach(function(item) {
- //   item.onclick = function(e) {
- //     $("#imageHolder").empty();
- //     $(".overlay").show();
- //     document.getElementById('imageHolder').innerHTML = this.innerHTML;
- //   }
- // });
-
- // $("#cheat-sheet-button").click(function() {
- //   $(".overlay").show();
- //   $(".cheat-sheet").show();
- // })
- //
- // $(".close-overlay").click(function() {
- //   $(".overlay").hide();
- //   $(".video").hide();
- //   $(".cheat-sheet").hide();
- // })
+document.getElementById("closeoverlay").onclick = function() {
+  document.getElementById('overlay').style.display = 'none';
+}
